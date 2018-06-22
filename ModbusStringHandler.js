97970011f46a89811daac36894c2f79b0e95901f
Tian Chengyuan <395750080@qq.com>
2018-06-22 09:40:56 +0800

    ddd

---
+++ ModbusStringHandler.js
@@ -0,0 +1,122 @@
+import crc from "crc";
+// this.pad = function() {
+//   var tbl = [];
+//   return function(num, n,encode=10,_switch=false) {
+//     var len = n-num.toString(encode).length
+//     if (len <= 0) return num.toString(encode)
+//     if (!tbl[len]) tbl[len] = (new Array(len+1)).join('0')
+//     let rtn = tbl[len] + num.toString(encode)
+//     return _switch ? (rtn[2]+rtn[3]+rtn[0]+rtn[1]) : rtn
+//   }
+// }()
+class ModbusStringHandler {
+  constructor(){
+    this.tbl=[]
+  }
+  CMD_ReadCoils(unityId,start,number){
+    //unityId,0x01,Hi起始，Lo起始，Hi数量，Lo数量
+    let cmd = ''
+    cmd += this.Pad(unitId,2)
+    cmd += '01'
+    cmd += this.Pad(start,4)
+    cmd += this.Pad(number,4)
+    return this.CalcModbusCrc(cmd)
+  }
+  CMD_ReadDiscreteInput(unitId,start,number){
+    //unityId,0x02,Hi起始，Lo起始，Hi数量，Lo数量
+    let cmd = ''
+    cmd += this.Pad(unitId,2)
+    cmd += '02'
+    cmd += this.Pad(start,4)
+    cmd += this.Pad(number,4)
+    return this.CalcModbusCrc(cmd)
+  }
+  CMD_ReadInputRegisters(unitId,start,number){
+    //unitId,0x03,Hi起始，Lo起始，Hi数量，Lo数量
+    let cmd = ''
+    cmd += this.Pad(unitId,2)
+    cmd += '04'
+    cmd += this.Pad(start,4)
+    cmd += this.Pad(number,4)
+    return this.CalcModbusCrc(cmd)
+  }
+  CMD_ReadHoldingRegisters(unitId,start,number){
+    //unitId,0x03,Hi起始，Lo起始，Hi数量，Lo数量
+    let cmd = ''
+    cmd += this.Pad(unitId,2)
+    cmd += '03'
+    cmd += this.Pad(start,4)
+    cmd += this.Pad(number,4)
+    return this.CalcModbusCrc(cmd)
+  }
+  ResponseHandler(response){
+    response = response.trim()
+    let unitId = parseInt(response.slice(0,2),16)
+    let funCode = parseInt(response.slice(2,4),16)
+    let numOfBytes = parseInt(response.slice(4,6),16)
+    let data = []
+    let dataPayload = response.slice(6,6+2*numOfBytes)
+    switch (funCode){
+      case (1):
+      case (2):
+        // dataPayload = dataPayload.slice(0,2*numOfBytes)
+        dataPayload = parseInt(dataPayload,16).toString(2)
+        // print(dataPayload)
+        dataPayload = dataPayload.split('').reverse()
+        for (let i = 0; i < numOfBytes; i++){
+          data.push(dataPayload.splice(0,8))
+        }
+        data = data.map(e => e.reverse())
+        let temp = []
+        data.forEach(e => { temp = temp.concat(e) })
+        data = temp.reverse()
+        data = data.map( e => {
+          switch (e){
+            case '0':
+              return false
+              break
+            case '1':
+              return true
+              break
+            default :
+              break
+          }
+        })
+        break;
+      case (3):
+      case (4):
+        for (let i = 0; i < numOfBytes/2; i++){
+          data.push(parseInt(dataPayload.slice(0,4),16))
+          dataPayload = dataPayload.slice(4)
+        }
+        break
+      default:
+        break
+    }
+    // for (let i = 0; i < numOfBytes/2; i++){
+    //   data.push(parseInt(dataPayload.slice(0,4),16))
+    //   dataPayload = dataPayload.slice(4)
+    // }
+    return {unitId,funCode,numOfBytes,data}
+  }
+  Pad(num, n,encode=16,_switch=false){
+    var len = n-num.toString(encode).length
+    if (len <= 0) return num.toString(encode)
+    if (!this.tbl[len]) this.tbl[len] = (new Array(len+1)).join('0')
+    let rtn = this.tbl[len] + num.toString(encode)
+    return _switch ? (rtn[2]+rtn[3]+rtn[0]+rtn[1]) : rtn
+  }
+  CalcModbusCrc(text){
+    let result = crc.crc16modbus(Buffer.from(text.trim(),'hex')).toString(16)
+    let ret = text+result[2]+result[3]+result[0]+result[1]
+    return ret
+  }
+  Str2Hex(str){
+    return Buffer.from(str.trim(),'hex')
+  }
+  Hex2Str(hex){
+    return hex.toString('hex')
+  }
+}
+
+export default ModbusStringHandler
---
+++ client.js
@@ -0,0 +1,25 @@
+require("babel-register")
+global.print = console.log
+var net = require('net');
+var ModbusStringHandler = require('./ModbusStringHandler')
+let handler = new ModbusStringHandler.default()
+print(handler.CalcModbusCrc('010300000100'))
+
+// print(handler.Pad(16*16,4,16))
+// for(i=0;i<parseInt('100',16);i++){
+//   print(handler.Pad(i,4,16,true))
+// }
+var socket = net.createServer()
+socket.listen(6790,'218.23.124.104')
+
+socket.on('connection',()=>{
+  print('connected')
+  print(handler.CMD_ReadHoldingRegisters(1,16,16))
+  print(handler.ResponseHandler('010306010000ff0010'))
+  print(handler.ResponseHandler('010103cd6b05'))
+  print(handler.ResponseHandler('010203cd6b05'))
+  clientSocket.write(handler.Str2Hex('000100000006'+handler.CalcModbusCrc('010300000001')))
+})
+socket.on('data',(data)=>{
+  print(data)
+})
